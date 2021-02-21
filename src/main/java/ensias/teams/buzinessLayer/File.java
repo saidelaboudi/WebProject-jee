3de947664e8a1b1ef3d8115574daf453b83a291f
saidelaboudi <sdelaboudi@gmail.com>
2021-02-21 04:37:19 +0100

    LGTM

--- src/main/java/ensias/teams/buzinessLayer/File.java
+++
@@ -1,9 +0,0 @@
-package ensias.teams.buzinessLayer;
-
-public class File extends SubChannel {
-
-	public File(String name) {
-		super(name);
-	}
-
-}
--- src/main/java/ensias/teams/buzinessLayer/MenuItem.java
+++
@@ -1,9 +0,0 @@
-package ensias.teams.buzinessLayer;
-
-public abstract class MenuItem {
-	public String itemName;
-	
-	public MenuItem(String name) {
-		this.itemName= name;
-	}
-}
