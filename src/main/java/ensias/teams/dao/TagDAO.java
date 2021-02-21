1c446f169e68b225c49790376f27a12cbdfa24f5
saidelaboudi <sdelaboudi@gmail.com>
2021-02-21 14:01:46 +0100

    LGTM

--- src/main/java/ensias/teams/dao/TagDAO.java
+++ src/main/java/ensias/teams/dao/TagDAO.java
@@ -8,22 +8,9 @@ import ensias.teams.buzinessLayer.User;
 /**
  * TagDAO
  */
-public class TagDAO {
+public interface TagDAO {
 
-    public void addTag_User(Tag t,DataBase db) throws SQLException{
-		int UserID;
-		String sql="INSERT INTO alias(Alias,StudentID) VALUES (?,?)";
-        PreparedStatement statement = db.connection.prepareStatement(sql);
-        
-        for(User user:t.tagged) {
-        	ResultSet set = db.Select("Users","Email='"+user.email+"'");
-        	set.next();
-        	UserID=set.getInt("ID");
-        	statement.setString(1,t.tagName);   
-        	statement.setInt(2, UserID);        	
-        	statement.execute();
-        }
-	}
+    public void addTag_User(Tag t,DataBase db) throws SQLException;
 
     
 }
