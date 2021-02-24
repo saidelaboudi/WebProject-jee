68dd0133ac020b6fe31b71462251194786f9582e
saidelaboudi <sdelaboudi@gmail.com>
2021-02-24 15:07:22 +0100

    LGTM

--- src/main/java/ensias/teams/dao/UserDao.java
+++ src/main/java/ensias/teams/dao/UserDao.java
@@ -6,4 +6,13 @@ import ensias.teams.buzinessLayer.User;
 
 public interface UserDao {
 	public ArrayList<User> bringAllUsers();
+	public User bringUser(String email, String pass) ;
+	public void addUser(User user);
+	public void addUser(User user,DAOFactory db) throws SQLException;
+	public ArrayList<User> addExcell2Depart(String excelFilePath) throws IOException, SQLException;
+	public ArrayList<User> addExcell2Depart(InputStream inputStream) throws IOException, SQLException;
+	public ArrayList<User> getUsersByTag(Tag tag1,DAOFactory db) throws SQLException;
+	public int getUserID(User user,DAOFactory db) throws SQLException;
+	public User getUserByID(int UserId,DAOFactory db) throws SQLException;
+	
 }
